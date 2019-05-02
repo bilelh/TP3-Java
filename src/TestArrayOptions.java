79a1baae562e36b2809a04462a0b31398d8df2e0
bilel <bilelh532@gmail.com>
2019-05-02 17:20:16 +0200

    Ajout de l'exo 4 TestArrayOptions git status

---
+++ src/TestArrayOptions.java
@@ -0,0 +1,20 @@
+
+public class TestArrayOptions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] array = {} ;
+		
+		System.out.println("1 . Vous avez le choix entre 2 options : ");
+		System.out.println("1 . Ajouter un nombre ");
+		System.out.println("2 . Afficher les nombres existants ");
+		System.out.println("Veuillez selectionner une option : ");
+		
+		
+		
+		
+
+	}
+
+}
